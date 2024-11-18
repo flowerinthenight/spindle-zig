817582a9500cdd8ae9f97c9fa5c6300d2220c9e0
flowerinthenight <root@flowerinthenight.com>
2024-11-18 09:23:48 +0900

    attempt build again, not working

--- build-deps-root.sh
+++ build-deps-root.sh
@@ -9,32 +9,29 @@ cd deps/abseil-cpp/ && rm -rfv cmake-out/ && \
         -DABSL_BUILD_TESTING=OFF \
         -DABSL_PROPAGATE_CXX_STD=ON \
         -DBUILD_SHARED_LIBS=yes \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -S . -B cmake-out && \
     cmake --build cmake-out -- -j $(nproc) && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # protobuf
 cd deps/protobuf/ && rm -rfv cmake-out/ && \
     cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_PREFIX_PATH=${ROOT_DIR}/deps/usr \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -DBUILD_SHARED_LIBS=yes \
         -Dprotobuf_BUILD_TESTS=OFF \
         -Dprotobuf_ABSL_PROVIDER=package \
         -S . -B cmake-out && \
     cmake --build cmake-out -- -j $(nproc) && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # grpc
 cd deps/grpc/ && rm -rfv cmake-out/ && \
     cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_PREFIX_PATH=${ROOT_DIR}/deps/usr \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -DBUILD_SHARED_LIBS=yes \
         -DgRPC_INSTALL=ON \
         -DgRPC_BUILD_TESTS=OFF \
@@ -47,14 +44,13 @@ cd deps/grpc/ && rm -rfv cmake-out/ && \
         -S . -B cmake-out && \
     cmake --build cmake-out -- -j $(nproc) && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # crc32c
 cd deps/crc32c/ && rm -rfv cmake-out/ && \
     cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_PREFIX_PATH=${ROOT_DIR}/deps/usr \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -DBUILD_SHARED_LIBS=yes \
         -DCRC32C_BUILD_TESTS=OFF \
         -DCRC32C_BUILD_BENCHMARKS=OFF \
@@ -62,27 +58,25 @@ cd deps/crc32c/ && rm -rfv cmake-out/ && \
         -S . -B cmake-out && \
     cmake --build cmake-out -- -j $(nproc) && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # nlohmann_json library
 cd deps/json/ && rm -rfv cmake-out/ && \
     cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_PREFIX_PATH=${ROOT_DIR}/deps/usr \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -DBUILD_SHARED_LIBS=yes \
         -DBUILD_TESTING=OFF \
         -DJSON_BuildTests=OFF \
         -S . -B cmake-out && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # opentelemetry-cpp
 cd deps/opentelemetry-cpp/ && rm -rfv cmake-out/ && \
     cmake \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_PREFIX_PATH=${ROOT_DIR}/deps/usr \
-        -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr \
         -DBUILD_SHARED_LIBS=yes \
         -DWITH_EXAMPLES=OFF \
         -DWITH_ABSEIL=ON \
@@ -91,13 +85,13 @@ cd deps/opentelemetry-cpp/ && rm -rfv cmake-out/ && \
         -DOPENTELEMETRY_ABI_VERSION_NO=2 \
         -S . -B cmake-out && \
     cmake --build cmake-out --target install -- -j $(nproc) && \
+    ldconfig && \
     cd ${ROOT_DIR}/
 
 # google-cloud-cpp
 cd google-cloud-cpp/ && rm -rfv cmake-out/ && \
     cmake -S . -B cmake-out \
           -DCMAKE_BUILD_TYPE=Release \
-          -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/deps/usr/ \
           -DBUILD_TESTING=OFF \
           -DGOOGLE_CLOUD_CPP_WITH_MOCKS=OFF \
           -DGOOGLE_CLOUD_CPP_ENABLE_EXAMPLES=OFF \
